5cdc2901658a026cb9b90ed72a4b4cfc4837dafd
Orson Peters <orsonpeters@gmail.com>
2020-02-23 17:44:21 +0100

    Added code for task 1.

---
+++ assignment1/task1.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+
+
+
+def euclidean_distance(a, b):
+    return np.sum((a - b)**2, axis=1)**0.5
+
+class Cloud:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+    @classmethod
+    def from_points(cls, points):
+        center = points.mean()
+        radius = np.max(euclidean_distance(points, center))
+        return cls(center, radius)
+
+    def is_in_cloud(self, points):
+        return euclidean_distance(points, self.center) <= self.radius
+
+
+if __name__ == "__main__":
+    train_X = pd.read_csv("data/train_in.csv", header=None)
+    train_y = pd.read_csv("data/train_out.csv", header=None)
+
+    clouds = []
+    for digit in range(10):
+        is_sample_digit = train_y[0] == digit
+        num_samples = np.sum(is_sample_digit)
+        cloud = Cloud.from_points(train_X.loc[is_sample_digit])
+        n = np.sum(cloud.is_in_cloud(train_X))
+        clouds.append(cloud)
+        print(f"Digit {digit}, cloud has radius {cloud.radius},"
+            f" was formed from {num_samples} points but {n} can be found inside it")
+
+    cloud_centers = np.array([cloud.center for cloud in clouds])
+    cloud_center_distances = pd.DataFrame(data=[euclidean_distance(c, cloud_centers)
+                                                for c in cloud_centers])
+    print("\nCloud center pairwise distances:")
+    print(cloud_center_distances)
+
+    nonzero_distances = cloud_center_distances.stack() > 0
+    smallest = cloud_center_distances.stack()[nonzero_distances].idxmin()
+    print("Smallest distance between", smallest)
+
+
