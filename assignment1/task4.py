f13a02ffcd4d4d559604299fb7094c115a424607
Amin <moradi.amin74@gmail.com>
2020-03-22 18:37:50 +0100

    task 4 update

--- assignment1/task4.py
+++ assignment1/task4.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+import seaborn as sn
 
 from common import as_numpy, confusion_dataframe
 
@@ -21,25 +22,27 @@ class CoverTheoremPerceptron:
         self.n_in = X.shape[1]
         self.n_out = len(self.labels)
         self.lr = 1
+        # Initializing weights
         self.weights = np.random.normal(size=(self.n_in, 1))
         self.bias = np.random.normal(size=(1,1))
         self.iterations = 0
         has_mispredictions = True
         while has_mispredictions:
             self.iterations +=1
-            # print('self.iterations', self.iterations)
+            # Calculating the training prediction
             prediction = np.dot(X,self.weights)
-
+            # Assigning 1 and -1 based on the values.
             prediction[prediction < 0] = -1
             prediction[prediction >= 0] = 1
-            
             mispredicted = prediction.flatten() != y
+            # Checking whether any misprediction exists
             has_mispredictions = np.any(mispredicted)
             if has_mispredictions:
                 tmp = np.argwhere(mispredicted).flatten()
                 random_index_update = np.random.choice(tmp)
+                # Updating weights by the perceptron update rule.
                 self.weights += self.lr * np.dot(y[random_index_update], X[random_index_update][:,np.newaxis]) 
-
+        return self.iterations
     def predict(self, X):
         prediction = np.dot(X, self.weights) + self.bias
         
@@ -55,52 +58,81 @@ if __name__ == "__main__":
     y_train = pd.read_csv("data/train_out.csv", header=None)
     X_test = pd.read_csv("data/test_in.csv", header=None)
     y_test = pd.read_csv("data/test_out.csv", header=None)
+    data = []
 
-    X_train = as_numpy(X_train)
-    y_train = as_numpy(y_train).flatten()
-
-    X_test = as_numpy(X_test)
-    y_test = as_numpy(y_test).flatten()
-
-    y_label_idx, labels = pd.factorize(y_train, sort=True)
-
-    
-    label_points = {}
-    all_labels = {}
-    test_label_points = {}
-    test_all_labels = {}
-    for label_idx, label in enumerate(labels):
-        label_points[label] = X_train[y_label_idx == label_idx]
-        all_labels[label] = y_train[y_label_idx == label_idx]
-
-
-    test_y_label_idx, test_labels = pd.factorize(y_test, sort=True)
-    for label_idx, label in enumerate(test_labels):
-        test_label_points[label] = X_test[test_y_label_idx == label_idx]
-        test_all_labels[label] = y_test[test_y_label_idx == label_idx]
-
-
-
-    
-    keys_list = list(all_labels.keys())
+    keys_list = list(range(10))
     all_pairs = [(keys_list[k1], keys_list[k2]) for k1 in range(len(keys_list)) for k2 in range(k1+1,len(keys_list))]
+        
 
+    # ***************** Pair-wise comparison ***************
     for (first_label, second_label) in all_pairs:
-        train_images = np.concatenate((label_points[first_label], label_points[second_label]))
-        train_labels = np.concatenate((all_labels[first_label], all_labels[second_label]))
+        # Initialize model to fit later
         model = CoverTheoremPerceptron()
-        model.fit(train_images, train_labels, (first_label, second_label))
+
+        # Extract all images with their pair-wise comparison of each labels
+        is_c1_train = y_train[0] == first_label
+        is_c2_train = y_train[0] == second_label
+        is_c1_test = y_test[0] == first_label
+        is_c2_test = y_test[0] == second_label
+
+        # Extract all training images with their respective labels
+        c1_train_points = as_numpy(X_train.loc[is_c1_train])
+        c2_train_points = as_numpy(X_train.loc[is_c2_train])
+        c1_train_labels = np.ones((c1_train_points.shape[0],1)) * first_label
+        c2_train_labels = np.ones((c2_train_points.shape[0],1)) * second_label
+
+        # Building train with numpy array by contatenating arrays extracted above.
+        train_images = np.concatenate((c1_train_points, c2_train_points))
+        train_labels = np.concatenate((c1_train_labels, c2_train_labels))
+
+        # Save number of itration of further plotting the data.
+        n_iter = model.fit(train_images, train_labels, (first_label, second_label))
+        data.append((first_label, second_label, n_iter))
+
+        # Extract all test images with their respective labels
+        c1_test_points = as_numpy(X_test.loc[is_c1_test])
+        c2_test_points = as_numpy(X_test.loc[is_c2_test])
+        c1_test_labels = np.ones((c1_test_points.shape[0],1)) * first_label
+        c2_test_labels = np.ones((c2_test_points.shape[0],1)) * second_label
+
+        # Building test numpy array by contatenating arrays extracted above.
+        y_test_labels = np.concatenate((c1_test_labels, c2_test_labels), axis=0)
+        X_test_points = np.concatenate((c1_test_points, c2_test_points), axis=0)
+
+        # Model prediction over test sample points
+        y_test_pred = model.predict(X_test_points)
+        print("Checking between labels: ", first_label, " and, ", second_label)
+        print(f"Test accuracy {accuracy_score(y_test_labels, y_test_pred):.3%}")
+
+    # ***************** One against all comparison ***************
+    data = []
+    for c1 in range(10):
+        # Extract all images of one label, and all the others that are not that one.
+        is_c1 = y_train[0] == c1
+        is_c2 = y_train[0] != c1
+
+        # Extract all test images with their respective labels
+        c1_train_points = as_numpy(X_train.loc[is_c1])
+        c2_train_points = as_numpy(X_train.loc[is_c2])
+        c1_train_labels = np.ones((c1_train_points.shape[0],1)) * c1
+        c2_train_labels = np.zeros((c2_train_points.shape[0],1))
+
+        # Initializing the train numpy array
+        train_images = np.concatenate((c1_train_points, c2_train_points))
+        train_labels = np.concatenate((c1_train_labels, c2_train_labels))
         
-        n_first_label = test_label_points[first_label].shape[0]
-        n_second_label = test_label_points[second_label].shape[0]
+        model = CoverTheoremPerceptron()
+        # Fitting the model for and saving iteration for further plotting.
+        n_iter = model.fit(train_images, train_labels, (c1, 0))
+        data.append((c1, n_iter))
 
-        y_test = np.concatenate((first_label * np.ones((n_first_label,1)), second_label * np.ones((n_second_label,1))), axis=0)
-
-        
-        X_test = np.concatenate((test_label_points[first_label], test_label_points[second_label]), axis=0)
-        y_test_pred = model.predict(X_test)
-
-        print("Checking between labels: ", first_label, " and, ", second_label, ".")
-        print(f"Test accuracy {accuracy_score(y_test, y_test_pred):.3%}")
-
-        
+    # ****** Ploting with SeaBorn *********
+    to_df = np.zeros((1, 10))
+    for (c1, inter) in data:
+       to_df[0][c1] =  inter
+    df = pd.DataFrame(data=to_df,index=[''])
+    plt.figure(figsize = (10, 2))
+    chart = sn.heatmap(df.round(0).astype(int), annot=True, cmap="Blues", fmt='g')
+    chart.set_ylabel('Iteration',rotation=90)
+    chart.set_yticklabels(chart.get_yticklabels(), rotation=90)
+    plt.show()
