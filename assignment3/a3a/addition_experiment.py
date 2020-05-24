310e6dcc366de7a5bb75be094a78d634a0238ebe
maminio <me@mamin.io>
2020-05-24 22:46:38 +0200

    training update

--- assignment3/a3a/addition_experiment.py
+++ assignment3/a3a/addition_experiment.py
@@ -6,9 +6,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
+from datetime import datetime
 
 from charactertable import CharacterTable
+from keras.callbacks import ModelCheckpoint
+import itertools
+import csv
 
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
 
 class AdderExperimentFactory:
@@ -48,7 +56,7 @@ class AdderExperimentFactory:
             ans_digits = fmt.format(a + b, digits=self.digits+1, flag=flag)
 
             if self.paired:
-                query = "".join(zip(a_digits, b_digits))
+                query = "".join(a+b for a, b in zip(a_digits, b_digits))
             else:
                 query = a_digits + "+" + b_digits
 
@@ -76,6 +84,21 @@ class AdderExperimentFactory:
 
 
 def run_experiment(rnn, reversed, binary, paired, seed=42):
+    reversed_folder = "reversed" if reversed else "normal"
+    binary_folder = "binary" if binary else "integer"
+    paired_folder = "paired" if paired else "not_paired"
+    filepath="./checkpoints/" + rnn.__qualname__ + "/" + reversed_folder + "_" + binary_folder + "_" + paired_folder + ""
+    logdir = "logs/scalars/" + rnn.__qualname__ + "_" + reversed_folder + "_" + binary_folder + "_" + paired_folder + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=filepath,
+        save_weights_only=True,
+        mode='auto', save_freq='epoch',
+        save_best_only=False
+    )
+
+    callbacks_list = [model_checkpoint_callback, tensorboard_callback]
+
     fac = AdderExperimentFactory(reversed, binary, paired)
     X, y = fac.generate_full_dataset()
     # Use 5% for training.
@@ -84,11 +107,25 @@ def run_experiment(rnn, reversed, binary, paired, seed=42):
     X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.95, random_state=seed ^ 0xdeadbeef)
 
     model = fac.generate_model(rnn)
-    history = model.fit(X_train, y_train, batch_size=128, epochs=1, validation_data=(X_val, y_val))
+    history = model.fit(X_train, y_train, batch_size=128, epochs=200, validation_data=(X_val, y_val), callbacks=callbacks_list)
     train_error = fac.compute_errors(y_train, model.predict(X_train))
     full_dataset_error = fac.compute_errors(y, model.predict(X))
+    with open(f'./results.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([rnn.__qualname__, reversed, binary, paired, train_error, full_dataset_error])
     return train_error, full_dataset_error, history
 
 
 if __name__ == "__main__":
-    print(run_experiment(rnn=layers.LSTM, reversed=False, binary=True, paired=False))
+    networks = [
+        layers.LSTM, 
+        layers.GRU,
+        layers.SimpleRNN
+        ]
+    # Reverse, binary, paired
+    l=[False,True]
+    options = list(itertools.product(l,repeat=3))
+
+    for network in networks:
+        for (reverse, binary, paired) in options:
+            results = run_experiment(rnn=network, reversed=reverse, binary=binary, paired=paired)
